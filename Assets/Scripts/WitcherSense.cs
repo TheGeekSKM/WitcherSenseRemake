96fc955c538b20de4ba8212c6dc3d1678bc8b335
Sai Mangipudi <thegeekskm101@gmail.com>
2021-05-06 14:36:00 -0500

    Small Changes

--- Assets/Scripts/WitcherSense.cs
+++ Assets/Scripts/WitcherSense.cs
@@ -1,6 +1,5 @@
 ﻿using Cinemachine;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UIElements;
 
 public class WitcherSense : MonoBehaviour
@@ -13,21 +12,12 @@ public class WitcherSense : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] public float witcherFOV = 31f;
     [SerializeField] public bool witcherMode;
-    [SerializeField] PostProcessVolume postVolume;
-    
     float originalFOV;
-    DepthOfField dOV;
-    LensDistortion lDis;
-    ChromaticAberration chromAbb;
 
     private void Start()
     {
         witcherMode = false;
         originalFOV = cameraSettings.m_Lens.FieldOfView;
-        
-        postVolume.profile.TryGetSettings(out dOV);
-        postVolume.profile.TryGetSettings(out lDis);
-        postVolume.profile.TryGetSettings(out chromAbb);
     }
 
 
@@ -47,38 +37,14 @@ public class WitcherSense : MonoBehaviour
         {
             if (cameraSettings.m_Lens.FieldOfView > witcherFOV)
             {
-                cameraSettings.m_Lens.FieldOfView -= 0.5f;
-            }
-
-            dOV.active = true;
-
-            if (lDis.intensity.value > -50f)
-            {
-                lDis.intensity.value -= 2f;
-            }
-
-            if (chromAbb.intensity.value < 1f)
-            {
-                chromAbb.intensity.value += 0.1f;
+                cameraSettings.m_Lens.FieldOfView -= 0.1f;
             }
         }
         else
         {
             if (cameraSettings.m_Lens.FieldOfView < originalFOV)
             {
-                cameraSettings.m_Lens.FieldOfView += 0.5f;
-            }
-
-            dOV.active = false;
-
-            if (lDis.intensity.value < 0f)
-            {
-                lDis.intensity.value += 2f;
-            }
-
-            if (chromAbb.intensity.value > 0f)
-            {
-                chromAbb.intensity.value -= 0.1f;
+                cameraSettings.m_Lens.FieldOfView += 0.1f;
             }
         }
     }
