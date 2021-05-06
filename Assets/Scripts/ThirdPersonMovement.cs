b25ba7e622c863d04997858c4e129d2773d53188
Sai Mangipudi <thegeekskm101@gmail.com>
2021-05-05 23:10:35 -0500

    Character Controller Part 2
    Finished up with adding gravity as well as a jump mechanic. The Third Person Camera and Character Controller are now done.

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,17 +5,32 @@ public class ThirdPersonMovement : MonoBehaviour
     [Header("Connections")]
     [SerializeField] CharacterController controller;
     [SerializeField] Transform cam;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+   
 
     [Header("Player Attributes")]
     [SerializeField] public float speed = 6f;
     [SerializeField] float turnSmoothTime = 0.1f;
     [SerializeField] float gravity = -9.81f;
+    [SerializeField] public float jumpHeight = 3f;
 
     float turnSmoothVelocity;
     Vector3 currentVelocity;
-    
+    bool isGrounded;
+
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && currentVelocity.y < 0)
+        {
+            currentVelocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -33,6 +48,11 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            currentVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         currentVelocity.y += gravity * Time.deltaTime;
