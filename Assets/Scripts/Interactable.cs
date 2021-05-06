208ee44474ba8008eb7e6110452ef41b329f732a
Sai Mangipudi <thegeekskm101@gmail.com>
2021-05-06 13:51:06 -0500

    Got the Shader to Respond to WitcherSense
    The shader now turns on and off based on whether the RMB is held or not.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] Renderer render;
+    [SerializeField] WitcherSense wSense;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wSense.witcherMode == true)
+        {
+            render.material.SetFloat("_Transparency", 0.2f);
+        }
+        else if (wSense.witcherMode == false)
+        {
+            render.material.SetFloat("_Transparency", 0f);
+        }
+    }
+}
