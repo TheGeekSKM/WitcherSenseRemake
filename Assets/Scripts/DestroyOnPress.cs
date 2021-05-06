3f0c0baa454e3d7d1b384a0044200bb9855d6ab6
Sai <thegeekskm101@gmail.com>
2021-05-06 17:58:59 -0500

    Item Interactivity Prototype
    I was able to make an Item explode when interacted. I still need to work out a few issues, but it works. It's also scuffed beyond recognition.

---
+++ Assets/Scripts/DestroyOnPress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnPress : MonoBehaviour
+{
+    [SerializeField] DisplayTextQuest displayText;
+    [SerializeField] ParticleSystem explosionParticle;
+    [SerializeField] Renderer meshRender;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (displayText.playerNear == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //particle stuffs
+                explosionParticle.Play();
+                displayText.enabled = false;
+                meshRender.enabled = false;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DisplayTextQuest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayTextQuest : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TextMeshPro text;
+    [SerializeField] LayerMask whatIsPlayer;
+
+
+    public bool playerNear;
+    void Start()
+    {
+        text.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerNear = Physics.CheckSphere(transform.position, 2f, whatIsPlayer);
+
+        if (playerNear)
+        {
+            text.enabled = true;
+
+        }
+        else if (!playerNear)
+        {
+            text.enabled = false;
+        }
+    }
+}
